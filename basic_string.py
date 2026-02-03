2544f31567f2fb1becf63378263a6d7080e7dffa
janaelawady06 <janaelawady06@gmail.com>
2026-02-03 17:15:40 +0100

    string examples

---
+++ basic_string.py
@@ -0,0 +1,20 @@
+s1 = "Hello World"
+s2 = 'Hello World'
+print(s1)
+print(s2)
+s1 = ("WIFOIRENPGE") #you can put weird characters (whatever u want)
+print(s1)
+
+#we can index a screen
+s = "banana"
+print(s1) #indexinf starts from 0 so s1 is a not b
+print(s0) # b
+print(s10) #error no 10 letters
+print(s(10/2)) #not working cus it will be 5.0
+print(s(10//2)) #working cus its 5
+print(s-1) #the last one so a
+print(s-6) #same as s(0)
+
+#len function will tell you the amount of characters
+print(s len(s))
+
---
+++ string_operators.py
@@ -0,0 +1,9 @@
+# + will perform string concatenation
+s1 = "hello"
+s2 = "bye"
+print(s1+s2)
+print(s2+s1)
+print(s1+" "+s2+"!!")
+
+#you cab multiply a string by an integer
+print(3*s2) #nsync song
